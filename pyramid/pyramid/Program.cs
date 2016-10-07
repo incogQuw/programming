016c1b33a728c8bc3445f52ec36f48098f42e7ae
William Terry <Tezza48is@aol.com>
2016-10-07 14:37:38 +0100

    initial pyramid code from class

---
+++ pyramid/pyramid/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int height = 0;
+            bool valid = false;
+
+            while(!valid)
+            {
+                Console.Write("Enter the pyramid height: ");
+                string currentEntry = Console.ReadLine();
+                valid = Int32.TryParse(currentEntry, out height);
+
+                if (height <= 0 || height > 23)
+                    valid = false;
+            }
+
+            int hashCount = 2;
+
+            for (int i = 0; i < height; i++)
+            {
+                string line = "";
+                int spaces = height - hashCount + 1;
+                for (int j = 0; j < spaces; j++)
+                {
+                    line += " ";
+                }
+
+                for (int j = 0; j < hashCount; j++)
+                {
+                    line += "#";
+                }
+
+
+                Console.WriteLine(line);
+                hashCount++; 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
